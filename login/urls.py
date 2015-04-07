03189cf13de3f36832ac4f62b88a6b8707cfc680
KUSHAL <kushalchawla18@gmail.com>
2015-04-07 14:46:01 +0530

    Doctor login app removed

--- login/urls.py
+++ login/urls.py
@@ -4,10 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^login/authenticate',views.authenticate),
-<<<<<<< HEAD
 	url(r'^login/logout',views.logout),
-=======
-	url(r'^logout',views.logout),
->>>>>>> 68f0e24ea15b4068fd73b88fd0bdc26929927a58
     url(r'', views.index, name='index'),
     ]
--- login/views.py
+++ login/views.py
@@ -14,32 +14,19 @@ def index(request):
     context = RequestContext(request)
     if 'index' not  in request.session:
             return render_to_response('login/login.html', context)
-<<<<<<< HEAD
-    context_dict = {'username': request.session["index"]}
-    return render(request,'login/doctor_homepage.html', context_dict)
-
-def logout(request):
-    del request.session["index"]
-    return HttpResponseRedirect("/")
-=======
     else:
         username =request.session["index"]
         Object_Searched = Registration.objects.filter(username = username)
         Object_Searched = Object_Searched[0]
         Category = Object_Searched.category
-        if Category == 1:
-            message="Hi"
-            return render_to_response('login/base.html')
-
+        context_dict = {'username': request.session["index"]}
+        return render(request,'login/doctor_homepage.html', context_dict)
 
 
 def logout(request):
     if 'index'  in request.session:
          del request.session['index']    
     return HttpResponseRedirect("/")
-
->>>>>>> 68f0e24ea15b4068fd73b88fd0bdc26929927a58
-
 
 def authenticate (request):
     if request.method == 'POST':
@@ -53,22 +40,7 @@ def authenticate (request):
                 Category=Object_Searched.category
                 message=Object_Searched.id
                 request.session["index"]=Object_Searched.username
-<<<<<<< HEAD
-                if  Category==1:
-                    context = RequestContext(request)
-                    message= "Hi1"
-                    return HttpResponseRedirect("/")
-                    #return HttpResponse(Category)
-                    
-                else:
-                    message= "Hi"
-                    return HttpResponse(message)
-=======
-                context = RequestContext(request)
-                #return HttpResponse(Category)
                 return HttpResponseRedirect("/")
->>>>>>> 68f0e24ea15b4068fd73b88fd0bdc26929927a58
-
             else:
                 message = "Wrong Password"
                 return HttpResponse(message)  
