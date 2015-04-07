e9999191366b564d7300f712b21174d60ad8b16e
KUSHAL <kushalchawla18@gmail.com>
2015-04-08 00:24:49 +0530

    integrated front end and ajinkya's reception work

--- login/urls.py
+++ login/urls.py
@@ -5,10 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^login/authenticate$',views.authenticate),
 	url(r'^login/logout$',views.logout),
-<<<<<<< HEAD
 	url(r'^login/appointment$',views.call_appoint),
-=======
 	url(r'^login/recep_homepage$',views.recep_homepage),
->>>>>>> 782264e2ee584a1c9a44c7538a63947de612cd80
     url(r'^$', views.index, name='index'),
     ]
