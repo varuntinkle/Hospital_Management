ab5d3d81d309de43b31add475273d6df022ddb7e
ajinkya599 <ajinkya599@gmail.com>
2015-04-09 23:27:05 +0530

    integrated bootstrap with receptionist login

--- login/views.py
+++ login/views.py
@@ -68,7 +68,53 @@ def call_appoint(request):
     else:
         return render_to_response('login/appointment.html', context)
 
-def recep_homepage (request):
+
+def book_amb(request):
+    context = RequestContext(request)
+    if 'index' not  in request.session:
+        return render_to_response('login/login.html', context)
+    else:
+        Object_Searched = AmbulanceSchedule.objects.all()
+        context_dict = {'object_amb': Object_Searched}
+        return render(request,'login/ambulance.html', context_dict)
+               
+def set_amb_sch(request):
+    if request.method=="POST":
+        source=request.POST['source']
+        destination=request.POST['destination']
+        day_time=request.POST['DayTime']
+        purpose=request.POST['purpose']
+        present_date=datetime.datetime.now()
+        listed=day_time.split(",")
+        day=listed[0]
+        time=listed[1]
+        time_list=time.split(" ")
+        if (time_list[1]=="a.m."):
+            time=time_list[0]+":00"
+        else:
+            lst=time_list[0].split(":")
+            hours=str(int(lst[0])+12)
+            mins=lst[1]
+            time=hours+":"+mins+":00"
+        new_object=AmbulanceBooking(Source=source,Destination=destination,DateBooked=present_date,Purpose=purpose,Day=day,Time=time)
+        new_object.save()
+        search = AmbulanceSchedule.objects.filter(Day = day, Time = time)
+        if search:
+            search=search[0]
+            search.Count=search.Count+1
+            search.save()
+    return HttpResponseRedirect("/")
+    
+def call_recep_schedule(request):
+    context = RequestContext(request)
+    if 'index' not  in request.session:
+        return render_to_response('login/login.html', context)
+    else:
+        Access_Schedule = AmbulanceSchedule.objects.all()
+        context_dict = {'object_schedule': Access_Schedule}
+        return render(request,'login/recep_schedule.html', context_dict)
+
+def end_recep_schedule(request):
     if request.method == 'POST':
 
         if 'commit' in request.POST:
@@ -96,35 +142,5 @@ def recep_homepage (request):
         elif 'reset' in request.POST:
             reset_day = request.POST['reset_day']
             AmbulanceSchedule.objects.filter(Day = reset_day).delete()
+            AmbulanceBooking.objects.filter(Day = reset_day).delete()
             return HttpResponseRedirect("/")
-
-def book_amb(request):
-    context = RequestContext(request)
-    if 'index' not  in request.session:
-        return render_to_response('login/login.html', context)
-    else:
-        Object_Searched = AmbulanceSchedule.objects.all()
-        context_dict = {'object_amb': Object_Searched}
-        return render(request,'login/ambulance.html', context_dict)
-               
-def set_amb_sch(request):
-    if request.method=="POST":
-        source=request.POST['source']
-        destination=request.POST['destination']
-        day_time=request.POST['DayTime']
-        purpose=request.POST['purpose']
-        present_date=datetime.datetime.now()
-        listed=day_time.split(",")
-        day=listed[0]
-        time=listed[1]
-        time_list=time.split()
-        time=time_list[0]+":00"
-        new_object=AmbulanceBooking(Source=source,Destination=destination,DateBooked=present_date,Purpose=purpose,Day=day,Time=time)
-        new_object.save()
-        search = AmbulanceSchedule.objects.filter(Day = day, Time = time)
-        if search:
-            search=search[0]
-            search.Count=search.Count+1
-            search.save()
-    return HttpResponseRedirect("/")
-    
