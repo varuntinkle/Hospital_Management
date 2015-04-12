6fcd478512e5c80d3f26e8bb9a6dc32e7bdacf5b
KUSHAL <kushalchawla18@gmail.com>
2015-04-12 14:40:22 +0530

    done

--- login/urls.py
+++ login/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	url(r'^login/ambulance$',views.book_amb),
 	url(r'^login/recep_submit$',views.end_recep_schedule),
 	url(r'^login/amb_submit$',views.set_amb_sch),
-	url(r'^noticeboard$',include('blog.urls')),
+	url(r'^noticeboard/',include('blog.urls')),
 	#url(r'^login/recep_homepage$',views.recep_homepage),
 	url(r'^$', views.index, name='index'),
     ]
--- login/views.py
+++ login/views.py
@@ -3,7 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from database.models import Registration, Patient, AmbulanceSchedule, AmbulanceBooking
+from database.models import Registration, Patient, AmbulanceSchedule, AmbulanceBooking, Post
 import django.contrib.auth.hashers
 from django.shortcuts import render
 import datetime
@@ -27,11 +27,13 @@ def index(request):
         elif(Category==2):
             object_pat = Patient.objects.filter(username = username)
             object_pat = object_pat[0]
-            context_dict = {'object_reg': Object_Searched,'object_pat': object_pat}
+            object_notice = Post.objects.all()[:5]
+            context_dict = {'object_reg': Object_Searched,'object_pat': object_pat, 'object_notice':object_notice}
             return render(request,'login/patient.html', context_dict)
         elif(Category==3):
             Access_Schedule = AmbulanceSchedule.objects.all()
-            context_dict = {'object_reg': Object_Searched, 'object_schedule': Access_Schedule}
+            object_notice = Post.objects.all()[:5]
+            context_dict = {'object_reg': Object_Searched, 'object_schedule': Access_Schedule, 'object_notice':object_notice}
             return render(request,'login/recep_homepage.html', context_dict)
 
 
@@ -64,7 +66,7 @@ def authenticate (request):
 def call_appoint(request):
     context = RequestContext(request)
     if 'index' not  in request.session:
-        return render_to_response('login/login.html', context)
+        return HttpResponseRedirect("/")
     else:
         return render_to_response('login/appointment.html', context)
 
@@ -72,7 +74,7 @@ def call_appoint(request):
 def book_amb(request):
     context = RequestContext(request)
     if 'index' not  in request.session:
-        return render_to_response('login/login.html', context)
+        return HttpResponseRedirect("/")
     else:
         Object_Searched = AmbulanceSchedule.objects.all()
         context_dict = {'object_amb': Object_Searched}
@@ -108,7 +110,7 @@ def set_amb_sch(request):
 def call_recep_schedule(request):
     context = RequestContext(request)
     if 'index' not  in request.session:
-        return render_to_response('login/login.html', context)
+        return HttpResponseRedirect("/")
     else:
         Access_Schedule = AmbulanceSchedule.objects.all()
         context_dict = {'object_schedule': Access_Schedule}
