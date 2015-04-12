f067c6738f0ce5995dd1a7cab94bf75643c1dc97
KUSHAL <kushalchawla18@gmail.com>
2015-04-12 13:51:36 +0530

    sync

--- Hospital_Management/urls.py
+++ Hospital_Management/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # url(r'^$', 'testing2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-     url(r'^blog/', include('blog.urls')),
+     url(r'^noticeboard/', include('blog.urls')),
     url(r'^', include('login.urls') ),
   
 ]
--- login/urls.py
+++ login/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
@@ -10,6 +10,7 @@ urlpatterns = [
 	url(r'^login/ambulance$',views.book_amb),
 	url(r'^login/recep_submit$',views.end_recep_schedule),
 	url(r'^login/amb_submit$',views.set_amb_sch),
+	url(r'^noticeboard$',include('blog.urls')),
 	#url(r'^login/recep_homepage$',views.recep_homepage),
 	url(r'^$', views.index, name='index'),
     ]
