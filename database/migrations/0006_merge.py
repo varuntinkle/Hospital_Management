8a26c355a651143886c2e7300fa7b984c1d6efc9
Varun Raj <varun.tinkle@gmail.com>
2015-04-20 00:25:42 +0530

    Images

---
+++ database/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('database', '0003_auto_20150420_1147'),
+        ('database', '0005_auto_20150419_1617'),
+    ]
+
+    operations = [
+    ]
